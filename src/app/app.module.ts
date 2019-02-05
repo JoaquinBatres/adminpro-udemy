b6a6a031cbb3a25188be078cba5c3e22474abf1e
JoaquinBatres <jbatres_cast@outlook.com>
2019-02-04 23:01:57 -0600

    v.1.1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './login/register.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { RegisterComponent } from './login/register.component';
     BrowserModule,
     AppRoutingModule,
     APP_ROUTES,
-    PagesModule
+    PagesModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,18 +1,23 @@
 import { NgModule } from "@angular/core";
 import { SharedModule } from '../shared/shared.module';
+import { FormsModule} from "@angular/forms"
 
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { Graficas1Component } from './graficas1/graficas1.component';
 import { ProgressComponent } from './progress/progress.component';
 import { PagesComponent } from './pages.component';
 import { PAGES_ROUTES } from './pages.routes';
+import { importType } from '@angular/compiler/src/output/output_ast';
+import { from } from 'rxjs';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
 
 @NgModule({
     declarations: [
         DashboardComponent,
         Graficas1Component,
         ProgressComponent,
-        PagesComponent
+        PagesComponent,
+        IncrementadorComponent
     ],
     exports: [
         DashboardComponent,
@@ -22,7 +27,8 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule
     ]
 })
 
